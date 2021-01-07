61e3728cd9414a972b54848be8ad2e2f3188a62e
BratiPradhan <pradhanbrati@gmail.com>
2021-01-07 20:17:31 +0530

    notes: integer type

--- WORKING WITH NUMBERS/main.cpp
+++ WORKING WITH NUMBERS/main.cpp
@@ -7,15 +7,27 @@ int main()
      ios::sync_with_stdio(0);
      cin.tie(0);
 
-    long long x = 123456789123456789LL;
-    cout << x << "\n";
+     long long x = 123456789123456789LL;
+     cout << x << "\n";
 
-   int a = 123456789;
-    long long b = (long long)a * a;
-    cout << "b=" << b ;
-    cout << "\n";
+     // WRONG OUTPUT
+     int p = 123456789;
+     long long q = p * p;
+     cout << 'Wrong output' << q << '\n';
 
-    cout << "Hello world!"
-         << "\n";
-    return 0;
+     // right output
+     int a = 123456789;
+     long long b = (long long)a * a;
+     cout << "b=" << b;
+     cout << "\n";
+
+     // right output
+     long long r = 123456789;
+     long long s = r * r;
+     cout << "s=" << s;
+     cout << "\n";
+
+     cout << "Hello world!"
+          << "\n";
+     return 0;
 }
--- WORKING WITH NUMBERS/notes.md
+++ WORKING WITH NUMBERS/notes.md
@@ -5,3 +5,14 @@
 | int          |    32    |  -2<sup>31</sup> ... 2<sup>31</sup> - 1  |   -2.10<sup>9</sup> ... 2.10<sup>9</sup> |
 | long long    |    64    |  -2<sup>63</sup> ... 2<sup>63</sup> - 1  | -9.10<sup>18</sup> ... 9.10<sup>18</sup> |
 | \_\_int128_t |   128    | -2<sup>127</sup> ... 2<sup>127</sup> - 1 |     -10<sup>38</sup> ... 10<sup>38</sup> |
+
+## WRONG OUTPUT
+
+     int p = 123456789;
+     long long q = p * p;
+     cout << 'Wrong output' << q << '\n';
+
+- expression p\*p will result type int
+- because of this q will have wrong output
+- change p's type to long long or
+- change expression to (long long)p\*p
